bec0fef4e120414793ff6168f4b3d832aba70b49
derevyannov <derevyannov@gmail.com>
2024-12-14 13:05:15 +0300

    class Question по порядку

--- question/Question.java
+++ question/Question.java
@@ -2,44 +2,10 @@ package question;
 
 public class Question {
     private String question;
-    public String getQuestion() {
-        return question;
-    }
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
     private String answer1;
-    public String getAnswer1() {
-        return answer1;
-    }
-    public void setAnswer1(String answer1) {
-        this.answer1 = answer1;
-    }
-
     private String answer2;
-    public String getAnswer2() {
-        return answer2;
-    }
-    public void setAnswer2(String answer2) {
-        this.answer2 = answer2;
-    }
-
     private String answer3;
-    public String getAnswer3() {
-        return answer3;
-    }
-    public void setAnswer3(String answer3) {
-        this.answer3 = answer3;
-    }
-
     private String answer4;
-    public String getAnswer4() {
-        return answer4;
-    }
-    private void setAnswer4(String answer4) {
-        this.answer4 = answer4;
-    }
     public int currentAnswer;
 
     public Question(String question, String answer1, String answer2, String answer3, String answer4, int currentAnswer) {
@@ -49,6 +15,37 @@ public class Question {
         this.answer3 = answer3;
         this.answer4 = answer4;
         this.currentAnswer = currentAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public String getAnswer1() {
+        return answer1;
+    }
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+    public String getAnswer2() {
+        return answer2;
+    }
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+    public String getAnswer3() {
+        return answer3;
+    }
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+    public String getAnswer4() {
+        return answer4;
+    }
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
     }
 }
 
